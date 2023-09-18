072dc27024b043e865449396be1c1c542f94c735
James Njenga <njengajames232@gmail.com>
2023-09-18 22:11:56 +0300

    initial commit

---
+++ madlibs/madlibs.py
@@ -0,0 +1,8 @@
+adj=input("Adjective: ")
+verb1=input("Verb: ")
+verb2=input("verb: ")
+famous_person=input("name: ")
+
+madlib = f"Computer programming is s0 {adj}! It makes me so excited all the time because \ I love to {verb1},stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
---
+++ set list tuple/set.py
@@ -0,0 +1,55 @@
+# a set is an unordered collection of unique elements
+# a set can be created in two ways
+# 1. using the set() function
+# 2. using curly braces {}
+
+set1 = set()
+print(set1)
+set1.add(1)
+print(set1)
+set1.add(2)
+print(set1)
+set1.add(2)
+print(set1)
+set1.add(3)
+print(set1)
+set1.add(4)
+print(set1)
+
+# OUTPUT:
+# set()
+# {1}
+# {1, 2}
+# {1, 2}
+# {1, 2, 3}
+# {1, 2, 3, 4}
+
+
+
+
+
+# we can also pass a list to the set() function
+set2 = set(['James', 'John', 'Peter', 'James', 'John', 'Peter','Martin'])
+print(set2)
+# OUTPUT:
+# {'John', 'James', 'Martin', 'Peter'}
+
+
+
+
+
+# we can also pass a string to the set() function
+set3 = set('James Jimmy Kinyenje')
+print(set3)
+# OUTPUT:
+# {'m', 'J', 'n', 'e', ' ', 'j', 'a', 'i', 's', 'K', 'y'}
+
+
+
+
+
+# we can also pass a tuple to the set() function
+set4 = set(('James', 'John', 'Peter', 'James', 'John', 'Peter','Martin'))
+print(set4)
+# OUTPUT:
+# {'James', 'Martin', 'Peter', 'John'}
